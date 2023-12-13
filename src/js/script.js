40bb05d188e5f0f8280e9f20604ee25fe40f1d7e
Rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2023-12-13 18:52:14 +0600

    build: Set the content theme for the Skills section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const viewportWidth = window.visualViewport.width
 const header = document.getElementById('header')
 
+
 // Scroll Action Function
 let lastScrolled = 0
 function scrollAction() {
@@ -17,3 +18,36 @@ function scrollAction() {
 }
 
 window.addEventListener('scroll', scrollAction)
+
+
+// Skills Table Feature
+const skillsTab = document.getElementById('skills-tab')
+const skillsTabBtns = skillsTab.querySelectorAll('button')
+const skillsTabIndicator = document.getElementById('skills-tab-indicator')
+const skillsBody = document.getElementById('skills-body')
+const skillsBodyBlock = skillsBody.querySelectorAll('div')
+
+// A function that moves a class between elements
+function displacement(elementRemove, elementAdd, desiredClass) {
+    elementRemove.classList.remove(desiredClass)
+    elementAdd.classList.add(desiredClass)
+}
+
+skillsTabBtns.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        let preActiveTab = skillsTab.querySelector('button.dynamic-style')
+        displacement(preActiveTab, element, 'dynamic-style')
+        let preActiveBody = skillsBody.querySelector('div.dynamic-style')
+        let activeBody = skillsBodyBlock[index]
+        displacement(preActiveBody, activeBody, 'dynamic-style')
+        if(viewportWidth > 768){
+            // For Desktop Devices Style
+            skillsTabIndicator.style.top = `calc(0px + ${index * 50}px)`
+        }else {
+            // For Mobile Devices Style
+            skillsTabIndicator.style.left = `calc(0px + ${index * 150}px)`
+        }
+    })
+    
+});
+
