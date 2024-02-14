e8f742ebdddf29d20a00d75a8ececcd5c7794602
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2024-02-14 12:50:31 +0600

    refactor: set archive page to load onto the home page
    And make the mobile menu of the archive page navigate through the home
    page

--- src/js/archive.js
+++ src/js/archive.js
@@ -1,5 +1,8 @@
 // Imports features like preloader and scrolled action
 import * as commonFeatures from './commonFeatures.js'
 
+// Scrolled to the top on refresh
+window.addEventListener('load', commonFeatures.refreshedTop)
+
 // Scrolled Feature
 window.addEventListener('scroll', commonFeatures.scrollAction)
